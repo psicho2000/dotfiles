e9965dc10ca1de118e50bd5eeda205336964fccc
Markus Meier <markus.d.meier@gmail.com>
2017-07-12 12:31:48 +0000

    adding first set of dotfiles

---
+++ symlink-dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dev="$HOME"
+dotfiles="$dev/dotfiles"
+
+if [[ -d "$dotfiles" ]]; then
+  echo "Symlinking dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+link() {
+  from="$1"
+  to="$2"
+  echo "Linking '$from' to '$to'"
+  rm -f "$to"
+  ln -s "$from" "$to"
+}
+
+for location in $(find $dotfiles -name '.*'); do
+  file="${location##*/}"
+  file="${file%.sh}"
+  link "$location" "$HOME/$file"
+done
