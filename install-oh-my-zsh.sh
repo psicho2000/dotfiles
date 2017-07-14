a94bfbad0e36be93b8d9a71a2dbc7a56b7e43a40
Markus Meier <markus.d.meier@gmail.com>
2017-07-14 21:43:53 +0200

    configure zsh

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -9,4 +9,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # set theme to ys
 echo "Set zsh theme to 'ys'"
-sed -i -E 's/ZSH_THEME=.*/ZSH_THEME="ys"/g' .zshrc
+sed -i -E 's/ZSH_THEME=.*/ZSH_THEME="ys"/g' ~/.zshrc
+
+# add plugins
+echo "Adding plugins: history colorize colored-man-pages common-aliases dircycle sudo"
+sed -i -E 's/plugins=.*/plugins=\(git history colorize colored-man-pages common-aliases dircycle sudo\)/g' ~/.zshrc
+
+# define nano as default editor
+echo "Define nano as default editor"
+echo "export EDITOR='nano'" >> ~/.zshrc
