ca12a0f162ffc457b66dd9e1d64b97aa572a35e7
Markus Meier <markus.d.meier@gmail.com>
2017-07-14 19:12:40 +0200

    add oh-my-zsh

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -8,4 +8,5 @@ cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # set theme to ys
+echo "Set zsh theme to 'ys'"
 sed -i -E 's/ZSH_THEME=.*/ZSH_THEME="ys"/g' .zshrc
