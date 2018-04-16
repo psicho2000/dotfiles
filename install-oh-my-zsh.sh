1c92b0f5a58a11515771f728949beee9e76a5b12
Markus Meier <markus.d.meier@gmail.com>
2018-04-16 14:56:25 +0200

    fix sed replacement for zsh plugins

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -11,8 +11,11 @@ echo "Set zsh theme to 'ys'"
 sed -i -E 's/ZSH_THEME=.*/ZSH_THEME="ys"/g' ~/.zshrc
 
 # add plugins
-echo "Adding plugins: colorize colored-man-pages common-aliases dircycle sudo"
-sed -i -E 's/plugins=.*/plugins=\(git history colorize colored-man-pages common-aliases dircycle sudo\)/g' ~/.zshrc
+## WARNING - this sed multiline replacement is greedy. Sed does not support non greedy searching.
+## If any other closing bracket ) will be added later to .zshrc, the sed replacement will not work anymore.
+echo "Adding plugins: git history colorize colored-man-pages common-aliases dircycle sudo"
+sed -n '1h;1!H;${;g;s/\nplugins=(.*)/\nplugins=(git history colorize colored-man-pages common-aliases dircycle sudo)/g;p;}' ~/.zshrc > ~/.zshrc-temp
+mv -f ~/.zshrc-temp ~/.zshrc
 
 # Add custom configuration
 echo "source ~/.zsh_custom" >> ~/.zshrc
